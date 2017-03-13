6436f924ae94c5caa222696a5c9fdb075c2f64e1
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-03-13 15:29:16 +0000

    reverting render stuff

--- src/render.js
+++ src/render.js
@@ -1,14 +1,5 @@
 import mainTemplate from './src/templates/main.html!text'
-import xr from 'xr'
-import config from './../config.json'
-import Mustache from 'mustache'
-//import swig from 'swig'
-import chambertemplate from './src/templates/chamber.html'
-
-function baseversion() {
-    return mainTemplate;
-}
 
 export async function render() {
-    return baseversion();
+    return mainTemplate;
 }
